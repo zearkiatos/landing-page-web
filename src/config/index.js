ae293547883815ea3e58e1b877f8e65adc938ff3
zearkiatos <caprilespe@outlook.com>
2023-06-25 19:49:17 -0400

    Api consume

---
+++ src/config/index.js
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const config = {
+  YOUTUBE_RAPID_API_BASE_URL: process.env.YOUTUBE_RAPID_API_BASE_URL,
+  YOUTUBE_CHANNEL_ID: process.env.YOUTUBE_CHANNEL_ID,
+  RAPID_API_KEY: process.env.RAPID_API_KEY,
+};
+
+export default config;
